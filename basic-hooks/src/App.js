59a6a300fc1bf55aeea11b483af9772daad1f8b9
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-12-22 13:07:02 +0900

    Added useState

---
+++ basic-hooks/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+function App() {
+  const [count, setCount] = useState(4)
+
+  function decrementCount(){
+    setCount(previousCount => previousCount - 1)
+  }
+
+  function incrementCount(){
+    setCount(previousCount => previousCount + 1)
+  }
+  return (
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  );
+}
+
+export default App;
