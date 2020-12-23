4e3a75c4b15269e276931e2935b47da36e7fe7f9
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-12-23 23:38:08 +0900

    Added useReducer

--- src/useFetchJob.js
+++ src/useFetchJob.js
@@ -1,3 +1,25 @@
+import { useReducer } from 'react'
+
+const ACTIONS = {
+    NAME_REQUEST = 'make-request',
+    GET_DATA = 'get-data',
+    ERROR: 'error'
+}
+
+function reducer(state,action) {
+    switch(action.type){
+        case ACTIONS.NAME_REQUEST:
+            return {...state, loading: true, jobs: []}
+        case ACTIONS.GET_DATA:
+            return {...state, jobs: false, jobs: action.payload.jobs}
+        case ACTIONS.ERROR:
+            return {...state, error: false, jobs: []}
+        default:
+            return state
+        
+    }
+}
+
 export default function useFetchJob(param,page){
     return{
         jobs: [1,2,4],
